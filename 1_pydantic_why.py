5f1441540eac268e89b813693a215ecee521d0d6
somnathbedia <somnathbedia7@gmail.com>
2026-02-01 20:37:33 +0530

    AnyHttpUrl type

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,26 +1,31 @@
-from pydantic import BaseModel
-from typing import List,Dict
+from pydantic import BaseModel,EmailStr,AnyHttpUrl
+from typing import List,Dict,Optional
 
-
+# Data validation
 # Pydantic model 
 class Patient(BaseModel):
     name: str
+    email:EmailStr
+    linkedin_url:AnyHttpUrl
     age: int
     weight:float
-    married: bool
+    married: Optional[bool] = None
     allergies: List[str]
     contact_details: Dict[str,str]
                       #    k   v 
+
+                    
 def insert_patient(patient:Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.email)
     print(patient.weight)
     print(patient.contact_details)
     print("Data inserted successfully!")
 
 
-patient_info = {'name':'Somnath','age':20,'weight':50.5,'married':1,'allergies':['pollen','dust'],'contact_details':{'email':'somnathbedia7@gmail.com','phone':'128745765'}}
+patient_info = {'name':'Somnath','email':'somnath@gmail.com','linkedin_url':'https://www.linkedin.com/in/somnath-bedia-640178225/','age':20,'weight':50.5,'married':1,'allergies':['pollen','dust'],'contact_details':{'phone':'128745765','address':'Vill - Paglapur, PO-LacchiPur'}}
 patient1 = Patient(**patient_info)
 
 
