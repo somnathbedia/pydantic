42ef76ee2fe25ded152cf3b2dfb89c3fec734315
somnathbedia <somnathbedia7@gmail.com>
2026-02-01 21:30:19 +0530

    custom field validator

---
+++ 3_model_validator.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel,EmailStr,field_validator
+from typing import List,Dict
+
+class Patient(BaseModel):
+    name:str
+    email:EmailStr
+    age:int
+    weight:float
+    married:bool
+    allergies:List[str]
+    contact_details:Dict[str,str]
