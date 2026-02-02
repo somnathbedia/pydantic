9d775f6f29382c4acff6daf7edc69b2c686bb1e6
somnathbedia <somnathbedia7@gmail.com>
2026-02-02 22:28:26 +0530

    Serialization

---
+++ 6_serialization.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel,EmailStr,field_validator
+from typing import List,Dict
+
+class Patient(BaseModel):
+    name:str
+    email:EmailStr
+    age:int
+    weight:float
+    married:bool
+    allergies:List[str]
+    contact_details:Dict[str,str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+        
+        valid_domains=['hdfc.com','icici.com']
+
+        domain_name=value.split('@')[-1]
+        
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
+        
+        return value
+    
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+    
+    #  field_validator can be operates in two modes
+    @field_validator('age',mode='after')
+    @classmethod
+    def validate_age(cls,value):
+        if 0<value<100:
+            return value
+        else:
+            raise ValueError('Age should be in between 0 and 100')
+    
+
+def save_patient(patient:Patient):
+
+    print(patient.name)
+    print(patient.email)
+    print(patient.allergies)
+
+    print("patient record inserted successfully!")
+
+
+patient_info = {'name':'Somnath','email':'somnath@hdfc.com','age':'20','weight':50.5,'married':1,'allergies':['pollen','dust'],'contact_details':{'phone':'128745765','address':'Vill - Paglapur, PO-LacchiPur'}}
+
+patient1 = Patient(**patient_info) #validation->Type coersion
+
+
+# temp = patient1.model_dump()
+temp = patient1.model_dump(include=['name','email'])
+
+
+
+print(temp)
+print(type(temp))
+
